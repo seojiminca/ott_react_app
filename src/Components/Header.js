3fea50eb5922e1bd5aa3dad819b0f7bc25ad06a6
jimin <seojiminca@gmail.com>
2020-04-11 16:54:29 +0900

    add styled-reset

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+//styled를 위해 이름을 변경
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+  
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export default () => (
-    <header>
-        <ul>
-            <li>
-                <Link to="/">Movies</Link>
-            </li>
-            <li>
-                <Link to="/tv">TV</Link>
-            </li>
-            <li>
-                <Link to="/search">Search</Link>
-            </li>
-        </ul>
-    </header>
+    <Header>
+        <List>
+            <Item>
+                <SLink to="/">Movies</SLink>
+            </Item>
+            <Item>
+                <SLink to="/tv">TV</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">Search</SLink>
+            </Item>
+        </List>
+    </Header>
 );
