745943eb68cd023156cbce8b2c897f76ffe71e54
jimin <seojiminca@gmail.com>
2020-04-11 16:29:48 +0900

    add styled-components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,17 +3,21 @@ import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom"
 import Home from "../Screen/Home";
 import Search from "../Screen/Search";
 import Tv from "../Screen/TV";
+import Header from "./Header";
 
 //단순 기능만 필요해서 class형으로 사용하지않아도 된다.
 // export.default 를 이용해서 바로 내보낸다.
 // 화면이동. url은 소문자
 export default () => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/search" exact component={Search} />
-            <Route path="/tv" exact component={Tv} />
-            <Redirect from="*" to="/" />
-        </Switch>
+        <>
+            <Header/>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/search" exact component={Search} />
+                <Route path="/tv" exact component={Tv} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 )
