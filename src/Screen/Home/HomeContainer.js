c285c88e9e0ccb3ebb4a5558f1027bf3950a337f
jimin <seojiminca@gmail.com>
2020-04-18 15:22:39 +0900

    add state #1

--- src/Screen/Home/HomeContainer.js
+++ src/Screen/Home/HomeContainer.js
@@ -2,14 +2,29 @@ import React, {Component} from 'react';
 import HomePresenter from "./HomePresenter";
 
 class HomeContainer extends Component {
+
+    // 상태, 함수, 라이프사이클함수.
+    state = {
+        nowPlaying: [],
+        upcoming: [],
+        popular: [],
+        loading: true,
+        error: null
+    }
+
     render() {
+
+        // 화면에 보여줄 상태나 함수값을 재선언하는 위치.
+        const {nowPlaying, upcoming, popular, loading, error} = this.state;
+
+        //리턴은 실제 화면에 보여지는 부분.
         return (
             <HomePresenter
-                nowPlaying={}
-                upcoming={}
-                popular={}
-                loading={}
-                error={}
+                nowPlaying={nowPlaying}
+                upcoming={upcoming}
+                popular={popular}
+                loading={loading}
+                error={error}
             />
         );
     }
