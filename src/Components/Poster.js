3029e96ea6a6bc39c7db34a41e5bcb8d50a47d13
jimin <seojiminca@gmail.com>
2020-04-18 18:45:00 +0900

    add Poster styling

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -2,15 +2,64 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
 
+const Container = styled.div`
+  font-size: 12px;
+`;
+
+
+const Image = styled.div`
+  background-image: url(${props => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 4px;
+  background-position: center;
+  transition: opacity 0.1s linear;
+`;
+
+
+const Rating = styled.span`
+  bottom: 10px;
+  right: 10px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
+
+//image, Rating 보다 밑에있어야 이미지를 불러올수있다.
+const ImgContainer = styled.div`
+  margin-bottom: 7px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
+
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255,255,255,0.5);
+`;
+
 const Poster = ({id, imgUrl, title, year, rating}) => (
-    <div>
-        <div>
-            <div></div>
-            <span>{rating} / 10</span>
-        </div>
-        <span>{title}</span>
-        <span>{year}</span>
-    </div>
+    <Container>
+        <ImgContainer>
+            <Image bgUrl={
+                `https://image.tmdb.org/t/p/w300${imgUrl}`
+            }/>
+            <Rating>⭐️ {rating} / 10</Rating>
+        </ImgContainer>
+        <Title>{title.slice(0, 18)}...</Title>
+        <Year>{year}</Year>
+    </Container>
 );
 
 Poster.propTypes = {
