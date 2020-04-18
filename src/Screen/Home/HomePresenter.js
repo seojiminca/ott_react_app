f209d808cc933faddf39315fd475d811a14819e6
jimin <seojiminca@gmail.com>
2020-04-18 17:42:33 +0900

    add Section component

--- src/Screen/Home/HomePresenter.js
+++ src/Screen/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
 import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
 
 // 2. 상수형함수 선언.
 const HomePresenter = ({nowPlaying, upcoming, popular, loading, error}) =>
@@ -13,22 +14,29 @@ const HomePresenter = ({nowPlaying, upcoming, popular, loading, error}) =>
     ) : (
         //여러개의 객체를 div로 묶어준다.
         <div>
-            {/*더세세한 조건일뿐 map으로 바로 뿌려줘도된다*/}
-            {nowPlaying && nowPlaying.length > 0 &&
-                nowPlaying.map(movie =>
-                    <span key={movie.id}>{movie.title}</span>
-                )
-            }
-            <p>구분선.</p>
-            {upcoming &&
-                upcoming.map(movie =>
-                    <span key={movie.id}>{movie.title}</span>
-                )
-            }
-            <p>구분선.</p>
-            {popular.map(movie =>
-                <span key={movie.id}>{movie.title}</span>
+            {/*더세세한 조건일뿐 map으로 바로 뿌려줘도된다. 여기선 3가지 다 사용함 */}
+            {nowPlaying && nowPlaying.length > 0 && (
+                // Section에서 미리 지정해둔 title과 그 밑의 내용 children.
+                <Section title="현재상영작">
+                    {nowPlaying.map(movie =>
+                        <span key={movie.id}>{movie.title}</span>
+                    )}
+                </Section>
             )}
+
+            {upcoming && (
+                <Section title="상영예정작">
+                    {upcoming.map(movie =>
+                        <span key={movie.id}>{movie.title}</span>
+                    )}
+                </Section>
+            )}
+
+            <Section title="인기작">
+                {popular.map(movie =>
+                    <span key={movie.id}>{movie.title}</span>
+                )}
+            </Section>
         </div>
     )
 
