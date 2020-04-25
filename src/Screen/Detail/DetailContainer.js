bc1ae0e1bd5d80f98fd0f613e03cd375c58fb8f3
jimin <seojiminca@gmail.com>
2020-04-25 17:30:31 +0900

    add backdrop

--- src/Screen/Detail/DetailContainer.js
+++ src/Screen/Detail/DetailContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import DetailPresenter from "./DetailPresenter";
+import {moviesApi, tvApi} from "../../API";
 
 
 //Detail에는 여러가지의 다양한 기능이 존재하니까 class형으로 사용.
@@ -14,10 +15,42 @@ class DetailContainer extends Component {
         this.state = {
             result: null, //movie or tv의 데이터.
             error: null,
-            loading: true,
+            loading: true, //data를 기다려야되는게 맞으니까 true
             isMovie: pathname.includes("/movie/") //domain에 movie가 들어가 있으면 true
         }
     }
+
+    async componentDidMount() {
+
+        //id를 서버에 push
+        const {
+            match: {
+                params: {id} //paramater 에 던지는 id
+            },
+            history: {push}
+        } = this.props;
+        const {isMovie} = this.state;
+        const parsedId = parseInt(id);
+        if(isNaN(parsedId)) {
+            return push("/");
+        }
+        //위에 result없으면 이거 let result = null;
+        let result = null;
+
+        //네트워크.
+        try{
+            if(isMovie){
+                ({ data: result} = await moviesApi.movieDetail(parsedId));
+            }else {
+                ({ data: result} = await tvApi.tvDetail(parsedId));
+            }
+        }catch{
+            this.setState({error: "error발생."})
+        }finally {
+            this.setState({loading: false, result})
+        }
+    }
+
 
     render() {
 
--- src/Screen/Detail/DetailPresenter.js
+++ src/Screen/Detail/DetailPresenter.js
@@ -4,16 +4,32 @@ import styled from "styled-components";
 import Loader from "../../Components/Loader";
 
 const Container = styled.div`
-
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  padding: 50px;
 `;
 
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${props => props.bgImage});
+  background-position: center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+  z-index: 0;
+`;
 
 const DetailPresenter = ({result, loading, error}) => (
     loading ? (
         <Loader/>
     ) : (
         <Container>
-
+            <Backdrop bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}/>
         </Container>
     )
 
